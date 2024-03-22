a283377e612d25fbc363fed0526c79cc8bdeb1e2
nguyenlam0306 <lamb2105548@student.ctu.edu.vn>
2024-03-22 23:02:04 +0700

    Hello

--- index.php
+++ index.php
@@ -1,17 +1,2 @@
-<?php
-echo "Hello Nhóm 4";
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang Chủ</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php require_once('config.php'); ?>
+<?php redirect('admin'); ?>
