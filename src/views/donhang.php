e7b78cc876f83c290f1b1c9f3b68cd035255d158
NguyenAnhHao1009 <anhhaoaoao@gmail.com>
2024-03-31 22:50:25 +0700

    2

--- src/views/donhang.php
+++ src/views/donhang.php
@@ -31,7 +31,7 @@ if (!isset($ds_donhang_timkiem)) {
         <form action="user_page.php?" method="GET" class="d-flex col-8 my-2" role="search">
             <input class="form-control me-2" type="text" placeholder="Nhập số điện thoại khách" name='timkiem-donhang' aria-label="Search" required>
             <button class="btn btn-outline-success" type="submit">Tìm</button>
-        </form>   
+        </form>
         <div class="text-end col-4">
             <a href="user_page.php?donhang=them" class="my-2 btn btn-success fw-bolder"><i class="fa-solid fa-file-circle-plus"></i> Tạo mới hóa đơn</a>
         </div>
@@ -42,7 +42,7 @@ if (!isset($ds_donhang_timkiem)) {
                 <th onclick="sortTable(0)">ID Hóa đơn <i href="" class=" fw-bolder"><i class="p-0 btn fa-solid fa-sort"></i></i></th>
                 <th onclick="sortTable(0)">Ngày tạo <i href="" class=" fw-bolder"><i class="p-0 btn fa-solid fa-sort"></i></th>
                 <th onclick="sortTable(2)">Người lập <i href="" class=" fw-bolder"><i class="p-0 btn fa-solid fa-sort"></i></th>
-                <th  onclick="sortTable(0)">Tên khách <i href="" class=" fw-bolder"><i class="p-0 btn fa-solid fa-sort"></th>
+                <th onclick="sortTable(0)">Tên khách <i href="" class=" fw-bolder"><i class="p-0 btn fa-solid fa-sort"></th>
                 <th onclick="sortTable(0)">Số tiền <i href="" class=" fw-bolder"><i class="p-0 btn fa-solid fa-sort"></th>
                 <th>Thao tác</th>
             </tr>
@@ -52,7 +52,7 @@ if (!isset($ds_donhang_timkiem)) {
                     <td><?= $hd[1] ?></td>
                     <td><?= $hd[2] ?></td>
                     <td><?= $hd[3] ?></td>
-                    <td><?= intval($hd[4]) ?>VNĐ</td>
+                    <td><?= intval($hd[4]) ?>Đánh dấu</td>
                     <td>
                         <a href="/user_page.php?donhang=xoa&id=<?= $hd[0] ?>"><i class="btn btn-outline-danger fa-solid fa-trash"></i></a>
                         <a href="/user_page.php?donhang=in&id=<?= $hd[0] ?>"><i class="btn btn-outline-primary fa-solid fa-print"></i></i></a>
